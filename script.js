3c1b0dac120b2057809e4b6c22b824316893f0e5
patbou76 <70040683+patbou76@users.noreply.github.com>
2021-06-07 14:31:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-body {background-color:black;font-family:arial;color:white}
-h1{color:grey;font-family:verdana}
-.maindiv{
-    background-color:white;
-    color:black;
-    width:300px;
-    padding:20px;
-    border:25px;
-    align:center
+
+// form validation / if ok call the compute function
+
+function validateF() {
+  var principalV = document.getElementById("principal").value;
+  if (principalV == "" || principalV < "1") {
+    alert("Please Enter a number greater or equal to 1");
+    document.getElementById("principal").focus();
+  }  else {
+
+    compute();
+  }
+}
+// Results of the interests amount calculation
+function compute()
+{
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate / 100;
+var year = new Date().getFullYear()+parseInt(years);
+var amount = interest + document.getElementById("principal").value;
+var amountR = parseFloat(amount).toFixed(2);
+
+   document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal
+   +"$\</mark\>,\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of \<mark\>"
+   +amountR+"$\</mark\>,\<br\>in the year "+year+"\<br\>"
+    
+}
+        
+
+// Slider for the interests rate
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+
 }
 
 
